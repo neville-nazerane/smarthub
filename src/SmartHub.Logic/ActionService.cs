3e5165a80bc73edf15d198fed811d16549b73efc
Neville <live.neville@live.com>
2020-11-15 22:42:48 -0700

    cancellation token

--- src/SmartHub.Logic/ActionService.cs
+++ src/SmartHub.Logic/ActionService.cs
@@ -8,6 +8,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace SmartHub.Logic
@@ -28,56 +29,55 @@ namespace SmartHub.Logic
             _logger = logger;
         }
 
-        public async ValueTask<IEnumerable<ActionInfo>> GetActionsInfo()
+        public async ValueTask<IEnumerable<ActionInfo>> GetActionsInfo(CancellationToken cancellationToken = default)
         {
             var ids = Enum.GetValues<ActionId>().Select(i => i.ToString());
             var actions = await _context.DeviceActions
                                         .Where(d => ids.Contains(d.Id))
                                         .Select(d => d.Id)
-                                        .ToArrayAsync();
+                                        .ToArrayAsync(cancellationToken);
 
             var result = ids.Select(id => new ActionInfo { Id = id, IsSet = actions.Contains(id) });
 
             return result;
         }
 
-        public async Task SetAsync(DeviceAction action)
+        public async Task SetAsync(DeviceAction action, CancellationToken cancellationToken = default)
         {
-            await using var transaction = await _context.Database.BeginTransactionAsync();
+            await using var transaction = await _context.Database.BeginTransactionAsync(cancellationToken);
 
             try
             {
-                await RemoveAsync(action.Id);
+                await RemoveAsync(action.Id, cancellationToken);
             }
             catch (Exception e)
             {
-                await transaction.RollbackAsync();
+                await transaction.RollbackAsync(cancellationToken);
                 _logger.LogError(e, $"Failed to remove '{action.Id}'");
                 return;
             }
 
-
-            await _context.DeviceActions.AddAsync(action);
+            await _context.DeviceActions.AddAsync(action, cancellationToken);
             try
             {
-                await _context.SaveChangesAsync();
+                await _context.SaveChangesAsync(cancellationToken);
             }
             catch (Exception e)
             {
-                await transaction.RollbackAsync();
+                await transaction.RollbackAsync(cancellationToken);
                 _logger.LogError(e, "Failed to add new action");
                 return;
             }
-            await transaction.CommitAsync();
+            await transaction.CommitAsync(cancellationToken);
         }
 
-        public async Task RemoveAsync(string id)
+        public async Task RemoveAsync(string id, CancellationToken cancellationToken = default)
         {
-            var record = await _context.DeviceActions.SingleOrDefaultAsync(d => d.Id == id);
+            var record = await _context.DeviceActions.SingleOrDefaultAsync(d => d.Id == id, cancellationToken);
             if (record is not null)
             {
                 _context.Remove(record);
-                await _context.SaveChangesAsync();
+                await _context.SaveChangesAsync(cancellationToken);
             }
             else
             {
