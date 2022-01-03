9b599cbc092717ea5f303be243d789755e07cb89
Neville Nazerane <live.neville@live.com>
2022-01-02 22:38:47 -0700

    settings crud

---
+++ src/SmartHub.Models/Entities/Setting.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace SmartHub.Models.Entities
+{
+
+    [Index(nameof(Name), IsUnique = true)]
+    public class Setting
+    {
+
+        public int Id { get; set; }
+
+        [MaxLength(20)]
+        public string Name { get; set; }
+
+        [MaxLength(20)]
+        public string Value { get; set; }
+
+    }
+}
