3d29ac3065998b2038fc6f17bdba02e086a15471
Neville Nazerane <live.neville@live.com>
2022-02-04 01:02:42 -0700

    archives setup

--- src/SmartHUb.BackupAzureFunctions/UploadBackup.cs
+++ src/SmartHUb.BackupAzureFunctions/UploadBackup.cs
@@ -8,14 +8,18 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using Microsoft.WindowsAzure.Storage.Blob;
+using Azure.Storage.Blobs;
+using System.Collections.Generic;
+using Azure.Storage.Blobs.Models;
 
 namespace SmartHub.BackupAzureFunctions
 {
     public static class UploadBackup
     {
+        private const string dateFormat = "yy-MM-dd-HH-mm";
 
         [FunctionName("UploadBackup")]
-        public static async Task<IActionResult> Run(
+        public static async Task<IActionResult> BackupEndpoint(
             [HttpTrigger(AuthorizationLevel.Function, "post", Route = "uploadBackup/{name}")] HttpRequest req,
             [Blob("backups/{name}.zip", FileAccess.Write)] Stream stream,
             ILogger log)
@@ -26,5 +30,38 @@ namespace SmartHub.BackupAzureFunctions
 
             return new OkObjectResult("Uploaded!");
         }
+
+        [FunctionName("BackupCleanupPolicy")]
+        public static async Task AutoCleanup(
+            [TimerTrigger("0 */1 * * * *", RunOnStartup = true)] TimerInfo timer
+            ,[Blob("backups", FileAccess.Read)]BlobContainerClient blobReader
+            ,[Blob("backups", FileAccess.Write)] BlobContainerClient backupClient
+            ,[Blob("archives", FileAccess.Write)] BlobContainerClient archiveClient
+            )
+        {
+
+            var pages = blobReader.GetBlobsAsync();
+
+            var blobsToMove = new List<BlobItem>();
+            var current = DateTime.Now;
+            await foreach (var blob in pages)
+            {
+                var date = DateTime.ParseExact(blob.Name, dateFormat, null);
+                if ((date - current).Days > 7)
+                {
+                    blobsToMove.Add(blob);
+                }
+            }
+
+            foreach (var blob in blobsToMove)
+            {
+                await using var stream = (await blobReader.GetBlobClient(blob.Name).DownloadStreamingAsync()).Value.Content;
+                await archiveClient.UploadBlobAsync($"Weekly-{blob.Name}", stream);
+                await backupClient.DeleteBlobAsync(blob.Name);
+            }
+
+
+        }
+
     }
 }
