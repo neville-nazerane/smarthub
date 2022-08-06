725ccfeb3e8a213419808f1e4e4e332b33f6c9bf
Neville Nazerane <live.neville@live.com>
2022-08-06 01:26:39 -0700

    web clean up

--- src/SmartHub.WebApp/Program.cs
+++ src/SmartHub.WebApp/Program.cs
@@ -2,25 +2,48 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using SmartHub.WebApp.Endpoints;
 
-namespace SmartHub.WebApp
+var builder = WebApplication.CreateBuilder(args);
+var Configuration = builder.Configuration;
+
+// SERVICES
+var services = builder.Services;
+
+services.AddApplicationInsightsTelemetry(Configuration["web_insights"]);
+services.AddCors()
+        .AddLogic(Configuration);
+
+
+var app = builder.Build();
+
+app.UseCors(b => b.AllowAnyHeader()
+                              .AllowAnyMethod()
+                              .WithOrigins(Configuration["clientUrl"].Split(",")));
+
+app.UseRouting();
+
+app.UseEndpoints(endpoints =>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+    endpoints.MapGet("/", context => context.Response.WriteAsync("Hello form another Device"));
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
-}
+    endpoints.MapBond();
+    endpoints.MapScenes();
+    endpoints.MapDevices("/devices");
+    endpoints.MapActions("/actions");
+    endpoints.MapHue("/hue");
+    endpoints.MapEventLogs("/logEvents");
+    endpoints.MapTriggers("/trigger");
+
+    endpoints.MapCrud("/settings", c => c.Settings);
+
+});
+
+await app.RunAsync();
