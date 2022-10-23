770f065778ffe63c501659e458297f8c89958784
Neville Nazerane <live.neville@live.com>
2022-10-22 18:48:22 -0700

    light endpoints

--- src/SmartHub.WebApp/Endpoints/BondEndpoints.cs
+++ src/SmartHub.WebApp/Endpoints/BondEndpoints.cs
@@ -14,7 +14,9 @@ namespace SmartHub.WebApp.Endpoints
             return new MultiEndpointConventionBuilder
             {
                 endpoints.MapPut("bond/fanSpeed/{fanId}/{speed}", SetFanSpeedAsync),
-                endpoints.MapGet("bond/fanSpeed/{fanId}", GetFanSpeedAsync)
+                endpoints.MapGet("bond/fanSpeed/{fanId}", GetFanSpeedAsync),
+                endpoints.MapPut("bond/light/{lightId}/{on}", SwitchLightAsync),
+                endpoints.MapGet("bond/light/{lightId}", GetLightStateAsync)
             };
         }
 
@@ -35,6 +37,15 @@ namespace SmartHub.WebApp.Endpoints
         {
             var status = await client.GetStateAsync(fanId, cancellationToken);
             return status.Power * status.Speed;
+        }
+
+        static Task SwitchLightAsync(string lightId, bool on, BondClient client, CancellationToken cancellationToken = default)
+            => client.ToggleLightAsync(lightId, on, cancellationToken);
+
+        static async Task<bool> GetLightStateAsync(string lightId, BondClient client, CancellationToken cancellationToken = default)
+        {
+            var status = await client.GetStateAsync(lightId, cancellationToken);
+            return status.Light == 1;
         }
 
     }
